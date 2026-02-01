5eedcf4631bcfe2ac395dba1e3fc796f02c96484
Rogerio Filho <rogerio.rhty@outlook.com>
2026-02-01 11:50:56 -0400

    Revert "last update on alpha things"
    This reverts commit a13c6e05f8d454f01efe4caaace039c8c0e874b7.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Cross Pong Remaster
-just a little project that I'm doing with a friend @thales_alex
+just a little project
--- Scripts/UI/vfx.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-@onready var animation := $AnimationPlayer
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> bool:
-	return true
